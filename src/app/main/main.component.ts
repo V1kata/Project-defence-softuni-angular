55bdaa914b40b3943011b3e143ba2281f503d471
V1kata <viktor.burboran@gmail.com>
2023-08-12 21:33:10 +0300

    Made a better start page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  currentTime: string = '';
 
+  ngOnInit(): void {
+    this.updateClock();
+  }
+
+  updateClock(): void {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    this.currentTime = `${this.formatTime(hours)}:${this.formatTime(
+      minutes
+    )}:${this.formatTime(seconds)}`;
+
+    setTimeout(() => this.updateClock(), 1000); // Update every second
+  }
+
+  formatTime(time: number): string {
+    return time < 10 ? `0${time}` : time.toString();
+  }
 }
