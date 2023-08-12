e3b589bec38bc00b107009ef1368538d5b1a69ec
V1kata <viktor.burboran@gmail.com>
2023-08-12 19:58:33 +0300

    environment setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    appUrl: 'https://parseapi.back4app.com',
+    appId: 'DP36CHk7g6purjlx4L2USmJ5F4YNQbvGaG38LsiN',
+    javascriptKey: 'fIUH1T7eRLcxJLZusRUrZMQW9dwiTiwhisPmAnah'
+};
