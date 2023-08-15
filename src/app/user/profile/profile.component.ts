f2710afd5fcc8087dc871fc9ef9a5214b667bc6e
V1kata <viktor.burboran@gmail.com>
2023-08-15 22:24:12 +0300

    Profile implement

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { ApiService } from 'src/app/api.service';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  creations: any = '';
+  profile: User | undefined = undefined;
+  constructor(private router: Router, private userService: UserService, private apiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.profile = this.userService.user;
+    this.apiService.getCatalog().subscribe({
+      next: (res: any) => {
+        this.creations = res.results.filter((item: any) => item.author.objectId === this.profile?.objectId)
+      }
+    })
+  }
 }
